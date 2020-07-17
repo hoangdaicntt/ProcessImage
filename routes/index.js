a2f68448854d6e3e5bc3e3e5d7208c069ba22ab1
hoangdaicntt <hoangdaicntt@gmail.com>
2020-07-17 09:53:19 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,33 @@ router.post('/api/crop', async function (req, res, next) {
     const pathOutput = 'public/images/output_' + id + '.jpeg';
     const pathInput = 'public/images/input_.' + id + url.split('.')[url.split('.').length - 1];
 
-    fs.writeFileSync(pathInput, await download(url));
-
-    const cropData = req.body.cropData;
-    const sizeOutput = req.body.sizeOutput;
-    const imageController = new ImageController(pathInput, pathOutput);
-    const result = await imageController.crop(cropData, sizeOutput).catch(err => {
-        console.log(err);
-        return null;
-    });
-    fs.unlinkSync(pathInput);
-    res.send({
-        path: 'https://learn-call.herokuapp.com/images/output_' + id + '.jpeg',
-        success: !!result
-    });
+    try {
+        fs.writeFileSync(pathInput, await download(url));
+        console.log(pathInput);
+        const cropData = req.body.cropData;
+        const sizeOutput = req.body.sizeOutput;
+        const imageController = new ImageController(pathInput, pathOutput);
+        const result = await imageController.crop(cropData, sizeOutput).catch(err => {
+            console.log(err);
+            return null;
+        });
+        fs.unlinkSync(pathInput);
+        res.send({
+            path: 'http://localhost:3000/images/output_' + id + '.jpeg',
+            success: !!result
+        });
+    } catch (e) {
+        res.send({
+            error: e,
+            path: null,
+            success: false
+        });
+    }
 });
 
 router.get('/api/delete', function (req, res, next) {
     let url = req.query.url;
-    const id = url.replace('https://learn-call.herokuapp.com/images/output_', '').replace('.jpeg', '');
+    const id = url.replace('http://localhost:3000/images/output_', '').replace('.jpeg', '');
     const pathOutput = 'public/images/output_' + id + '.jpeg';
     fs.unlinkSync(pathOutput);
     res.send({success: true});
